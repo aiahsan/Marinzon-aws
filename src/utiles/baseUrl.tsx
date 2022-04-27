27a9edf95f89c1f19c191921b8ad086455ec59f3
aiahsan <ai.ahsanismail@gmail.com>
2022-04-27 11:03:41 +0500

    app-update

--- src/utiles/baseUrl.tsx
+++ src/utiles/baseUrl.tsx
@@ -1,6 +1,6 @@
 import { create } from 'apisauce'
 // export const mainUrl="https://localhost:44370/"
-export const mainUrl="http://ec2-34-205-4-150.compute-1.amazonaws.com/"
+export const mainUrl="https://dpcare.co/marinzon/"
 export const ImageUrl=mainUrl+"wwwroot/Uploads/"
 export const api = create({
     baseURL: mainUrl+'api/',
